060c2b5cc5d454a66a0b3450ae1a07909319a4ff
GabrielLovato <gabriellcc20@gmail.com>
2026-02-22 17:45:38 -0300

    Update EnemyIA.cs

--- Lab2/Assets/EnemyIA.cs
+++ Lab2/Assets/EnemyIA.cs
@@ -5,18 +5,41 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public Transform puck; 
+    public float speed = 15f; 
     
-    public float speed = 5f; 
+    private Rigidbody2D rb2d;
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        startPosition = transform.position; 
+    }
 
     void Update()
     {
-        Vector2 targetPos = new Vector2(puck.position.x, transform.position.y);
+        Vector2 targetPos;
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (puck.position.y > 0)
+        {
+            targetPos = new Vector2(puck.position.x, puck.position.y);
+        }
+        else
+        {
+            targetPos = startPosition;
+        }
 
-        var pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, -2.5f, 2.5f); 
-        pos.y = Mathf.Clamp(pos.y, 0.2f, 4.5f);  
-        transform.position = pos;
+        targetPos.x = Mathf.Clamp(targetPos.x, -2.5f, 2.5f); 
+        targetPos.y = Mathf.Clamp(targetPos.y, 0.2f, 4.5f);  
+
+        Vector2 dir = targetPos - (Vector2)transform.position;
+
+        if (dir.magnitude < 0.1f)
+        {
+            rb2d.velocity = Vector2.zero;
+            return;
+        }
+
+        rb2d.velocity = dir.normalized * speed;
     }
 }
