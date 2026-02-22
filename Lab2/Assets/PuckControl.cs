7f27e3329c2f6426cf71234ac6696765ab6d097c
GabrielLovato <gabriellcc20@gmail.com>
2026-02-22 17:45:32 -0300

    add sound

--- Lab2/Assets/PuckControl.cs
+++ Lab2/Assets/PuckControl.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class PuckControl : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    public AudioSource source; 
 
     void Start()
     {  
         rb2d = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>(); 
     }
 
     public void ResetBall()
     {
         rb2d.velocity = Vector2.zero;
         transform.position = Vector2.zero;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) 
+    {
+        source.Play(); 
     }
 }
